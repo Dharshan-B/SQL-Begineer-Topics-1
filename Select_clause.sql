04a0cfe6943abc71a45bf8e455ad63c947022a0a
Dharshan B <109328490+Dharshan-B@users.noreply.github.com>
2025-06-24 16:44:04 +0530

    Create Select_clause.sql

---
+++ Select_clause.sql
@@ -0,0 +1,10 @@
+--Retrives the Whole Table with every columns 
+Select * from table_name; 
+--Retrives the certain colmns 
+Select col_name from table_name; 
+ -- Retrives Multiple Cols at Once 
+Select col1 ,
+col2,
+col3
+from table_name; 
+
