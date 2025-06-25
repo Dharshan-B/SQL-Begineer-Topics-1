722f1a16a6cd4dcc7e783a717cd41414b7532b8b
Dharshan B <109328490+Dharshan-B@users.noreply.github.com>
2025-06-25 12:31:43 +0530

    Create Distinct_cluase.sql

---
+++ Distinct_cluase.sql
@@ -0,0 +1,3 @@
+--this removes the Duplicates/repeated values 
+Select Distinct country from customers;
+--Dont use Distinct Unless if you have necessary with using Distinct because it may slow down the query
